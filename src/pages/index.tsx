9351bf6b40c05d38524d40157a7223ff77f50c53
Khurelbaatar Dorj <23LP5779@G304-CC0083.local>
2024-03-18 12:29:30 +0800

    ts

---
+++ src/pages/index.tsx
@@ -0,0 +1,38 @@
+export default function Home() {
+  let sideMenus = [
+    {
+      title: "string",
+      buttonText: "string",
+      contents: [
+        {
+          name: "string",
+          price: 123,
+          isVerified: false,
+        },
+      ],
+    },
+    {
+      title: "string",
+      buttonText: "string",
+      contents: [
+        {
+          name: "string",
+          price: 123,
+          isVerified: false,
+        },
+      ],
+    },
+    {
+      title: "string",
+      buttonText: "string",
+      contents: [
+        {
+          name: "string",
+          price: 123,
+          isVerified: false,
+        },
+      ],
+    },
+  ];
+  return <div></div>;
+}
